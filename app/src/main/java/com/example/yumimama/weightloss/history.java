f100b24a50d5119ae38b405fb8973b3aa525fca9
yzmaggie2008 <31428439+yzmaggie2008@users.noreply.github.com>
2018-02-05 22:28:26 -0800

    Add SQLite

--- app/src/main/java/com/example/yumimama/weightloss/history.java
+++ app/src/main/java/com/example/yumimama/weightloss/history.java
@@ -1,14 +1,19 @@
 package com.example.yumimama.weightloss;
 
 import android.content.Intent;
+import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.design.widget.BottomNavigationView;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.AdapterView;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.LinearLayout;
 import android.widget.ListView;
 import android.widget.TextView;
 import android.widget.Toast;
@@ -20,11 +25,13 @@ import java.util.List;
  * Created by yumimama on 1/17/18.
  */
 
-public class history extends AppCompatActivity {
-    private ListView lvHistory;
-    private historyListAdapter adapter;
-    private List<historyData> mHistoryList;
-    private TextView mTextMessage;
+public class history extends AppCompatActivity implements View.OnClickListener {
+    private ListView historys;
+    private HistoryAdapter adapter;
+    private Button btnAdd,btnSearch;
+    private DatabaseHandler dbHandler;
+    private List<historyData> historyList;
+    private SQLiteDatabase db;
 
 
     private BottomNavigationView.OnNavigationItemSelectedListener mOnNavigationItemSelectedListener
@@ -49,38 +56,121 @@ public class history extends AppCompatActivity {
             return false;
         }
     };
+
     @Override
-    protected void onCreate(@Nullable Bundle savedInstanceState) {
+    protected void onCreate( Bundle savedInstanceState) {
 
 
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_history);
 
-        mTextMessage = (TextView) findViewById(R.id.message);
-        BottomNavigationView navigation = (BottomNavigationView) findViewById(R.id.navigation);
-        navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
+        historys= (ListView) findViewById(R.id.history_list);
+        btnAdd= (Button) findViewById(R.id.btn_add);
+        btnSearch= (Button) findViewById(R.id.btn_search);
 
 
-        lvHistory = (ListView) findViewById(R.id.ls_history);
+        btnSearch.setOnClickListener(this);
+        btnAdd.setOnClickListener(this);
 
-        mHistoryList = new ArrayList<>();
+        dbHandler=new DatabaseHandler(this);
 
-        mHistoryList.add(new historyData(1,"12/28","165cm and 130lbs ", "Drink Water, Especially Before Meals"));
-        mHistoryList.add(new historyData(2,"12/30","165cm and 129lbs ", "Eat Eggs For Breakfast"));
-        mHistoryList.add(new historyData(3,"1/3","165cm and 129lbs ", "Drink Coffee (Preferably Black)"));
-        mHistoryList.add(new historyData(4,"1/5","165cm and 128lbs ", "Cook With Coconut Oil"));
-        mHistoryList.add(new historyData(5,"1/7","165cm and 125lbs ", "Take a Glucomannan Supplement"));
-        mHistoryList.add(new historyData(6,"1/9","165cm and 125lbs ", "Cut Back on Added Sugar"));
-        mHistoryList.add(new historyData(7,"1/11","165cm and 123lbs ", " Go on a Low Carb Diet"));
 
-        adapter = new historyListAdapter(getApplicationContext(),mHistoryList);
-        lvHistory.setAdapter(adapter);
+        historyList = dbHandler.getAllHistory();
+        adapter = new HistoryAdapter(this,historyList);
+        historys.setAdapter(adapter);
 
-        lvHistory.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+
+        historys.setOnItemClickListener(new AdapterView.OnItemClickListener() {
             @Override
-            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
-                Toast.makeText(getApplicationContext(), "If you want to return, click Return! and the history ID is: " + view.getTag(),Toast.LENGTH_SHORT).show();
+            public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
+                Intent intent=new Intent(history.this,HistoryActivity.class);
+
+
+                intent.putExtra("request","Look");
+                intent.putExtra("id",historyList.get(i).getId());
+                intent.putExtra("date",historyList.get(i).getDate());
+                intent.putExtra("grade",historyList.get(i).getWeight());
+
+                startActivityForResult(intent, 0);
             }
         });
     }
-}
+
+    @Override
+    public void onClick(View view) {
+        switch (view.getId()){
+            case R.id.btn_add:
+                Intent i=new Intent(history.this,HistoryActivity.class);
+
+                i.putExtra("request","Add");
+                startActivityForResult(i, 1);
+                break;
+            case R.id.btn_search:
+                AlertDialog.Builder builder=new AlertDialog.Builder(this);
+
+
+                final LinearLayout searchView= (LinearLayout) getLayoutInflater().inflate(R.layout.dialog_search,null);
+                builder.setView(searchView);
+                final AlertDialog dialog=builder.create();
+                dialog.show();
+
+
+                final EditText searchDate= (EditText) searchView.findViewById(R.id.search_date);
+                Button btnDialogSearch= (Button) searchView.findViewById(R.id.btn_search_dialog);
+                btnDialogSearch.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View view) {
+                        searchDate.setVisibility(View.GONE);
+                        ListView list = (ListView) searchView.findViewById(R.id.search_result);
+                        List<historyData> resultList = new ArrayList<historyData>();
+                        final historyData searchHistory = dbHandler.getHistory(searchDate.getText().toString());
+                        if (searchHistory != null) {
+                            resultList.add(searchHistory);
+                            HistoryAdapter resultAdapter = new HistoryAdapter(getApplicationContext(), resultList);
+                            list.setAdapter(resultAdapter);
+                            list.setVisibility(View.VISIBLE);
+                            list.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+                                @Override
+                                public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
+                                    dialog.dismiss();
+                                    Intent intent = new Intent(history.this, HistoryActivity.class);
+                                    intent.putExtra("request", "Look");
+                                    intent.putExtra("id", searchHistory.getId());
+                                    intent.putExtra("date", searchHistory.getDate());
+                                    intent.putExtra("weight", searchHistory.getWeight());
+                                    startActivityForResult(intent, 0);
+                                }
+                            });
+                        } else {
+                            //关闭Dialog
+                            dialog.dismiss();
+                            Toast.makeText(getApplicationContext(), "No data existed at that day", Toast.LENGTH_SHORT).show();
+                        }
+
+                    }
+                });
+                break;
+        }
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+
+
+        switch (requestCode){
+            case 0:
+                if (resultCode==2)
+                    Toast.makeText(this,"Saved Change",Toast.LENGTH_SHORT).show();
+                if (resultCode==3)
+                    Toast.makeText(this,"Already Delete",Toast.LENGTH_SHORT).show();
+                break;
+            case 1:
+                if (resultCode==RESULT_OK)
+                    Toast.makeText(this,"Add Successful",Toast.LENGTH_SHORT).show();
+                break;
+        }
+
+        onCreate(null);
+    }
+    }
+
--- app/src/main/java/com/example/yumimama/weightloss/historyData.java
+++ app/src/main/java/com/example/yumimama/weightloss/historyData.java
@@ -1,50 +1,49 @@
 package com.example.yumimama.weightloss;
 
+import java.io.Serializable;
+
 /**
  * Created by yumimama on 1/18/18.
  */
 
-public class historyData {
+public class historyData implements Serializable {
     private int id;
     private String date;
-    private String wh;
-    private String tips;
+    private String weight;
 
-    public  historyData(int id, String date, String wh, String tips){
+    public historyData(){}
+    public historyData(int id, String date, String weight) {
         this.id = id;
         this.date = date;
-        this.wh = wh;
-        this.tips = tips;
+        this.weight = weight;
+    }
+
+    public historyData(String date, String weight) {
+        this.date = date;
+        this.weight = weight;
     }
 
     public int getId() {
         return id;
-    }
-    public void setId(int id){
-        this.id = id;
     }
 
     public String getDate() {
         return date;
     }
 
+    public String getWeight() {
+        return weight;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
     public void setDate(String date) {
         this.date = date;
     }
 
-    public String getWh() {
-        return wh;
-    }
-
-    public void setWh(String wh) {
-        this.wh = wh;
-    }
-
-    public String getTips() {
-        return tips;
-    }
-
-    public void setTips(String tips) {
-        this.tips = tips;
+    public void setWeight(String weight) {
+        this.weight = weight;
     }
 }
